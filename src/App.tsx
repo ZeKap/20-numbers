d7321e95f1554e88aa6e58dfc2bcefcb814a67e5
ZeKap <kapismath@gmail.com>
2023-08-30 01:40:38 +0200

    First text

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,10 @@ import './App.css';
 function App() {
   return (
     <>
+      <h1>20 numbers sorted</h1>
+      <p>A random number from 0 to 1000 will be randomly picked, choose where to put it in this list so the list is sorted at the end.</p>
+      <p>WARNING! Once the place for the number is chosen, you can't move it.</p>
+
     </>
   );
 }
